b244252f4ba6e6a08c45dd92372057531192026b
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-05-21 02:14:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">
+        Rick and morty <span className="text-primary">Wiki</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
